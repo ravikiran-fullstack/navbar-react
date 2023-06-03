89eb06d9a036272a77094e1ae11c5c16dfd17399
Ravikira Yadava <ravikiranjobs21@gmail.com>
2023-06-03 16:19:25 -0400

    Completed responsive navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,4 @@
-import React, { react, useState } from 'react';
+import { useState } from 'react';
 
 import { FaBars } from 'react-icons/fa';
 import { links, social } from '../data';
@@ -20,19 +20,32 @@ const Navbar = () => {
             <FaBars />
           </button>
         </div>
-        {showLinks && (
-          <div className="links-container">
-            <ul className="links">
-              {links.map((link) => {
-                return (
-                  <li key={link.id}>
-                    <a href={link.url}>{link.text}</a>
-                  </li>
-                );
-              })}
-            </ul>
-          </div>
-        )}
+
+        <div
+          className={`links-container ${
+            showLinks ? 'show-links-container' : ''
+          }`}
+        >
+          <ul className="links">
+            {links.map((link) => {
+              return (
+                <li key={link.id}>
+                  <a href={link.url}>{link.text}</a>
+                </li>
+              );
+            })}
+          </ul>
+        </div>
+        <ul className="social-icons">
+          {social.map((socialIcon) => {
+            const { id, url, icon } = socialIcon;
+            return (
+              <li key={id}>
+                <a href={url}>{icon}</a>
+              </li>
+            );
+          })}
+        </ul>
       </div>
     </nav>
   );
